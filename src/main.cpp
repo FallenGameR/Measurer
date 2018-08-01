d841bd77fe1dd67928619f63a6599b819f21f592
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2018-07-31 22:29:51 -0700

    showing both temperatures

--- src/main.cpp
+++ src/main.cpp
@@ -197,27 +197,56 @@ void Graph(Adafruit_ILI9341 &d, double x, double y, double gx, double gy, double
 #define Y_AXES_PADDING 30
 #define TITLE_PADDING 30
 
+// the value of the 'other' resistor
+#define SERIESRESISTOR 1e4
+
+// Termistor resistance at 25c
+#define THERMISTORNOMINAL 1e4
+
+// temp. for nominal resistance (almost always 25 C)
+#define TEMPERATURENOMINAL 25
+
+// The beta coefficient of the thermistor (usually 3000-4000)
+#define BCOEFFICIENT 3950
+
+#define PIN_TERMISOTOR_ENABLE 7
+
 void drawTermistorReading()
 {
   boolean display7 = true;
 
   for (double x = 0; x <= 60; x += 1)
   {
-    int vo = analogRead(ADJ_PIN);
+    digitalWrite(PIN_TERMISOTOR_ENABLE, HIGH);
+    int vi = analogRead(ADJ_PIN);
+    digitalWrite(PIN_TERMISOTOR_ENABLE, LOW);
+    double r = SERIESRESISTOR / (1023.0 / vi - 1.0);
 
+    double steinhart;
+    steinhart = r / THERMISTORNOMINAL;                // (R/Ro)
+    steinhart = log(steinhart);                       // ln(R/Ro)
+    steinhart /= BCOEFFICIENT;                        // 1/B * ln(R/Ro)
+    steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+    steinhart = 1.0 / steinhart;                      // Invert
+    steinhart -= 273.15;                              // convert to C
+
+    /*
     //  Temp Kelvin
-    double tempK = log(10000.0 * ((1024.0 / vo - 1.0)));
+    double tempK = log(r);
     tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK)) * tempK);
 
     // Convert Kelvin to Celcius
     double y = tempK - 273.15;
 
-    //Serial.println(y);
+    */
+
+    double y = r;
+    Serial.println(y);
 
     Graph(
         tft,                                           // &d name of your display object
         x,                                             // x = x data point
-        y,                                             // y = y datapont
+        steinhart,                                     // y = y datapont
         X_AXES_PADDING,                                // gx = x graph location (lower left) - needed for y axes numbers
         tft.height() - Y_AXES_PADDING,                 // gy = y graph location (lower left) - needed for x axes numbers and title
         tft.width() - X_AXES_PADDING - 1,              // w = width of graph                 - needed for y and last x axes numbers
@@ -228,9 +257,9 @@ void drawTermistorReading()
         20,                                            // ylo = lower bound of y axis
         30,                                            // yhi = upper bound of y asis
         1,                                             // yinc = division of y axis (distance not count)
-        "Room Temperature",                            // title = title of graph
-        "Time [s]",                                    // xlabel = x asis label
-        "Temperature [deg C]",                         // ylabel = y asis label
+        "Temparature",                                 // title = title of graph
+        "seconds",                                     // xlabel = x asis label
+        "celcius",                                     // ylabel = y asis label
         DKBLUE,                                        // gcolor = graph line colors
         RED,                                           // acolor = axi ine colors
         GREEN,                                         // pcolor = color of your plotted data
@@ -273,6 +302,8 @@ void printDhtReadings()
   }
 
   bool needToErase = ((temperature != new_temperature) || (humidity != new_humidity));
+  needToErase = true;
+
   if (needToErase)
   {
     Serial.println("erase");
@@ -298,6 +329,9 @@ void printDhtReadings()
 void setup()
 {
   Serial.begin(9600);
+  //analogReference(EXTERNAL);
+
+  pinMode(PIN_TERMISOTOR_ENABLE, OUTPUT);
 
   tft.begin();
   tft.setRotation(1);
@@ -313,4 +347,6 @@ void setup()
 void loop(void)
 {
   drawTermistorReading();
+  printDhtReadings();
+  delay(1);
 }
