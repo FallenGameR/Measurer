fa8993449343416b98df2e09f04347ce0cceac03
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2018-08-05 19:18:51 -0700

    testing out mem consumption

--- src/main.cpp
+++ src/main.cpp
@@ -28,14 +28,25 @@ void setup()
 
 void loop()
 {
+  // DATA:    [========  ]  78.3% (used 1603 bytes from 2048 bytes)
+  // PROGRAM: [========= ] 91.7 % (used 29592 bytes from 32256 bytes)
   tft.fillScreen(BLACK);
+
+  // DATA:    [========  ]  77.0% (used 1577 bytes from 2048 bytes)
+  // PROGRAM: [========  ]  80.5% (used 25964 bytes from 32256 bytes)
   drawTermistorReading();
 
+  // DATA:    [======    ]  60.5% (used 1239 bytes from 2048 bytes)
+  // PROGRAM: [========= ]  87.8% (used 28310 bytes from 32256 bytes)
   PmRead();
 
+  // DATA:    [=======   ]  65.8% (used 1347 bytes from 2048 bytes)
+  // PROGRAM: [=======   ]  71.7% (used 23134 bytes from 32256 bytes)
   BmeRead();
   delay(2000);
 
+  // DATA:    [=======   ]  71.5% (used 1465 bytes from 2048 bytes)
+  // PROGRAM: [========= ]  88.1% (used 28410 bytes from 32256 bytes)
   RtcRead();
   delay(5000);
 }
