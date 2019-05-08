08d9287ee5036dd94598e2121845702ba0f9317a
Aleksandr Kostikov <Alex.Kostikov@gmail.com>
2019-05-07 22:00:54 -0700

    pins updated

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,12 @@
 // The control pins for the LCD can be assigned to any digital or
 // analog pins...but we'll use the analog pins as this allows us to
 // double up the pins with the touch screen (see the TFT paint example).
-#define LCD_CS A3 // Chip Select goes to Analog 3
-#define LCD_CD A2 // Command/Data goes to Analog 2
-#define LCD_WR A1 // LCD Write goes to Analog 1
-#define LCD_RD A0 // LCD Read goes to Analog 0
+#define LCD_CS A15 // Chip Select goes to Analog 3
+#define LCD_CD A14 // Command/Data goes to Analog 2
+#define LCD_WR A13 // LCD Write goes to Analog 1
+#define LCD_RD A12 // LCD Read goes to Analog 0
 
-#define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
+#define LCD_RESET A11 // Can alternately just connect to Arduino's reset pin
 
 // When using the BREAKOUT BOARD only, use these 8 data lines to the LCD:
 // For the Arduino Uno, Duemilanove, Diecimila, etc.:
